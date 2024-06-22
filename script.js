c8f70c1aaf6a0b227e44831c05355ba67987c276
Jyoti261298 <84282090+Jyoti261298@users.noreply.github.com>
2024-06-22 13:06:21 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let input = document.getElementById("task-input");
+let task = document.querySelector(".task");
+
+function Add() {
+  if (input.value == "") {
+    alert("Task cannot be empty");
+  } else {
+    let newEle = document.createElement("ul");
+    newEle.innerHTML = `${input.value} <input type ="checkbox">`;
+    task.appendChild(newEle); //add the new element in the task
+    input.value = ""; // empty's the input field
+    newEle.querySelector("input").addEventListener("click", strikeThrough);
+    function strikeThrough() {
+      if (this.checked) {
+        newEle.style.textDecoration = "line-through";
+        color = "black";
+      } else {
+        newEle.style.textDecoration = "none";
+      }
+    }
+  }
+}
